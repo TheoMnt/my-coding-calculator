83603fc82839893612fd2d3e8a9593f14d469608
Th.Manetas <manetas.th@gmail.com>
2024-12-25 13:55:25 +0200

    mod method created

--- src/gr/aueb/cf/calc/Main.java
+++ src/gr/aueb/cf/calc/Main.java
@@ -5,10 +5,18 @@ public class Main {
 
     }
     public static int add(int a, int b) {
+
         return a + b;
     }
     public static int sub(int a, int b) {
+
         return a - b;
     }
-    public static int div(int a, int b) { return a / b; }
+    public static int div(int a, int b) {
+        return a / b;
+    }
+    public static int mod(int a , int b) {
+        return a % b;
+    }
+
 }
